51e51e737b2a58bc9fa6a8d17ffcd5caa4c3dc31
Samad <Samad.o@outlook.com>
2026-04-18 11:07:00 -0400

    connected element to container

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let words = ["letter", "music", "craft"];
+const letterContainer = document.getElementById("letter-box");
 
 document.addEventListener("click", () => {
-  const randWord = words[Math.floor(Math.random() * words.length)];
+  let randWord = words[Math.floor(Math.random() * words.length)];
+
+  randWord = randWord.split("");
+
+  let array = randWord.map((value) => {
+    return `<span class ="${value}"></span>`;
+  });
+
+  let fullHtml = array.join("");
+  letterContainer.innerHTML = fullHtml;
+  letterContainer.querySelectorAll("span").forEach((word) => {
+    word.classList.add("concealed");
+  });
 });
