054c4c8f186475d820977e9d93f1b6ec12704b90
Samad <Samad.o@outlook.com>
2026-04-20 20:19:40 -0400

    dynamically start game and restart game.

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ const letterContainer = document.getElementById("letter-box");
 const lives = document.querySelector(".lives");
 let scoreTally = 0;
 let randWord;
+const gameStart = document.getElementById("game-start");
+const restart = document.getElementById("restart");
 
-document.addEventListener("click", () => {
-  randWord = words[Math.floor(Math.random() * words.length)];
-
+function fetchGame (){
+    randWord = words[Math.floor(Math.random() * words.length)];
+  gameStart.classList.add("hidden")
   randWord = randWord.split("");
 
   let array = randWord.map((value) => {
     return `<span id="${value}" class="concealed" ></span>`;
   });
 
+  
+for (let i=0; i<3; i++) {
+const hearts = document.createElement("img");
+console.log(hearts);
+hearts.src = "./imgs/heart-png-38780.png";
+hearts.classList.add("heart")
+lives.appendChild(hearts)
+}
+
   let fullHtml = array.join("");
   letterContainer.innerHTML = fullHtml;
-});
+}
+
+gameStart.addEventListener("click", fetchGame);
+restart.addEventListener("click", fetchGame);
+
 
 document.addEventListener("keyup", (e) => {
   letterContainer.querySelectorAll("span").forEach((word) => {
@@ -30,6 +45,9 @@ document.addEventListener("keyup", (e) => {
 
   if (!randWord.includes(e.key)) {
     const hearts = document.querySelector(".heart");
+    if (!hearts) return;
     hearts.remove();
   }
 });
+
+
