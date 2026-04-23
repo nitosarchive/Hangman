8b4974c25db303d7fc00298ad7b8ad004ec561f9
Samad <Samad.o@outlook.com>
2026-04-23 18:15:19 -0400

    minor styling changes

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-
 const letterContainer = document.getElementById("letter-box");
 const lives = document.querySelector(".lives");
 let scoreTally;
 const gameStart = document.getElementById("game-start");
 const restart = document.getElementById("restart");
 let hearts;
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const score = document.getElementById("score")
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const score = document.getElementById("score");
 let lastKeyCodes;
 let selectedWord;
-let randWord
+let randWord;
 
+fetch("./words.json")
+  .then((response) => response.json())
+  .then((words) => {
+    selectedWord = words;
+  });
 
-fetch('./words.json').then(response => response.json()).then(words => {
-  selectedWord = words
-})
-
-function fetchGame (){
-  
+function fetchGame() {
   document.querySelector(".intro-container").classList.add("hidden");
   document.querySelector(".game-container").classList.remove("hidden");
 
-  
-  randWord = selectedWord[Math.floor(Math.random() * selectedWord.length)]
+  randWord = selectedWord[Math.floor(Math.random() * selectedWord.length)];
   randWord = randWord.split("");
   scoreTally = 0;
   lastKeyCodes = [];
@@ -31,27 +56,22 @@ function fetchGame (){
     return `<span id="${value}" class="concealed letter" ></span>`;
   });
 
-  document.querySelectorAll(".heart").forEach((hearts)=>{
+  document.querySelectorAll(".heart").forEach((hearts) => {
     hearts.remove();
-  })
+  });
 
-
-    for (let i=0; i<5; i++) { 
+  for (let i = 0; i < 5; i++) {
     const heartImg = document.createElement("img");
     heartImg.src = "./imgs/heart-png-38780.png";
     heartImg.classList.add("heart");
     lives.appendChild(heartImg);
-
-
-
-}
+  }
   let fullHtml = array.join("");
   letterContainer.innerHTML = fullHtml;
 }
 
 gameStart.addEventListener("click", fetchGame);
 restart.addEventListener("click", fetchGame);
-
 
 document.addEventListener("keyup", (e) => {
   if (!alphabet.includes(e.key)) return;
@@ -62,18 +82,15 @@ document.addEventListener("keyup", (e) => {
     hearts = document.querySelector(".heart");
     if (!hearts) return;
     hearts.remove();
-  } 
+  }
 
   letterContainer.querySelectorAll("span").forEach((word) => {
     if (word.id === e.key) {
       word.classList.remove("concealed");
       scoreTally = scoreTally + 10;
-      
+
       score.innerText = scoreTally;
       word.innerText = e.key;
     }
   });
-
-
 });
-
