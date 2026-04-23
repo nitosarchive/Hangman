9d9e0732f9df9873d2d12e68d330d758858eef1e
Samad <Samad.o@outlook.com>
2026-04-23 17:43:13 -0400

    code refractoring, bigger words library for more diversity and working on fixing bugs

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-let words = ["letter", "music", "craft"];
+
 const letterContainer = document.getElementById("letter-box");
 const lives = document.querySelector(".lives");
 let scoreTally;
-let randWord;
 const gameStart = document.getElementById("game-start");
 const restart = document.getElementById("restart");
 let hearts;
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const score = document.getElementById("score")
 let lastKeyCodes;
+let selectedWord;
+
 
 function fetchGame (){
-  randWord = words[Math.floor(Math.random() * words.length)];
+  fetch('./words.json').then(response => response.json()).then(words => {
+  selectedWord = words
+})
+
   document.querySelector(".intro-container").classList.add("hidden");
   document.querySelector(".game-container").classList.remove("hidden");
-  randWord = randWord.split("");
+
+  
+  selectedWord = selectedWord[Math.floor(Math.random() * selectedWord.length)]
+  
+  selectedWord = selectedWord.split("");
   scoreTally = 0;
   lastKeyCodes = [];
   score.innerText = 0;
-  let array = randWord.map((value) => {
+  let array = selectedWord.map((value) => {
     return `<span id="${value}" class="concealed letter" ></span>`;
   });
 
@@ -27,7 +35,7 @@ function fetchGame (){
   })
 
 
-    for (let i=0; i<3; i++) { 
+    for (let i=0; i<5; i++) { 
     const heartImg = document.createElement("img");
     heartImg.src = "./imgs/heart-png-38780.png";
     heartImg.classList.add("heart");
@@ -47,15 +55,12 @@ restart.addEventListener("click", fetchGame);
 document.addEventListener("keyup", (e) => {
   if (!alphabet.includes(e.key)) return;
   if (lastKeyCodes.includes(e.keyCode)) return;
-  lastKeyCodes.push(e.keyCode);
-  if (!randWord.includes(e.key)) {
-    hearts = document.querySelector(".heart");
-  
-    
-    hearts.remove();
-    if (!hearts){
 
-    };
+  lastKeyCodes.push(e.keyCode);
+  if (!selectedWord.includes(e.key)) {
+    hearts = document.querySelector(".heart");
+    if (!hearts) return;
+    hearts.remove();
   } 
 
   letterContainer.querySelectorAll("span").forEach((word) => {
@@ -70,5 +75,4 @@ document.addEventListener("keyup", (e) => {
 
 
 });
-
 
